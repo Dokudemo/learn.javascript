145ec21ec5d0b152934915c136690d820e95055b
dokudev <dokudemo@proton.me>
2026-09-17 11:56:54 -0700

    solution

---
+++ data_types.js
@@ -0,0 +1,28 @@
+'use strict';
+/*
+
+В JS есть 8 типов данных из который только 7 примитивыне 
+1 является не примитивным это object
+1. number
+2. string
+3. BigInt
+4. undefined
+5. null
+6. symbol
+7. boolean
+8. object
+
+*/
+
+/*
+Что выведет этот скрипт?
+    let name = "Ilya";
+
+    alert( `hello ${1}` ); // ? hello 1
+
+    alert( `hello ${"name"}` ); // ? hello name
+
+    alert( `hello ${name}` ); // ? hello Ilya
+
+*/
+
